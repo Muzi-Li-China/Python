8570ea74f005eff3dc00bbd0f4614790eb16d988
Muzi-Li-China <38267715+Muzi-Li-China@users.noreply.github.com>
2018-04-21 11:55:49 +0800

    Update 2018-04-21-爬虫.py

--- 2018-04-21-爬虫.py
+++ 2018-04-21-爬虫.py
@@ -1,34 +1,34 @@
 import urllib.request
 import re
 import pandas as pd
-url = 'https://www.zhipin.com/c101250100/h_101250100/?query=%E6%95%B0%E6%8D%AE&page=1'  #网址
-request = urllib.request.Request(url)   #请求
-
-#爬取结果
-response = urllib.request.urlopen(request)
-data = response.read()
-#设置解码方式
-data = data.decode('utf-8')
-jobs = re.compile('<div class="job-title">(.+)</div>')  #岗位
-salary = re.compile('<span class="red">(.+)</span>')    #薪资
-regions = re.compile(r"""<p>(.+)  <em class="vline"></em>(.+)<em class="vline"></em>(.+)</p>
-                                    </div>""")          #地区，经验，学历
-jobslist = jobs.findall(data)       #岗位
-salarylist = salary.findall(data)   #薪资
-regionlist = regions.findall(data)   #地区，经验，学历
-region =[]
-experience = []
-education = []
-for i in regionlist:
-    region.append(i[0])
-    experience.append(i[1])
-    education.append(i[2])
-df = pd.DataFrame({'region':region,
-                   'Salary':salarylist,
-                   'Jobs':jobslist,
-                   'experience':experience,
-                   'education':education,
-                   })
-print(df)
-
+url = 'https://www.zhipin.com/c101250100/h_101250100/?query=%E6%95%B0%E6%8D%AE&page=1&ka=page-1'
+def request(url,n):
+    data = urllib.request.urlopen(urllib.request.Request(url)).read()
+    data = data.decode('utf-8')
+    # # 公司名称
+    # corporate_name_re = re.compile('target="_blank">(.+)</a></h3>')
+    # corporate_name_list = corporate_name_re(data)
+    #岗位
+    jobsre = re.compile('<div class="job-title">(.+)</div>')
+    jobslist = jobsre.findall(data)
+    #薪资
+    salaryre = re.compile('<span class="red">(.+)</span>')
+    salarylist = salaryre.findall(data)
+    #细节
+    detailre = re.compile('<p>(.+)  <em class="vline"></em>(.+)<em class="vline"></em>(.+)</p>')
+    detaillist = detailre.findall(data)
+    zonelist = [] #地区
+    experiencelist = [] #经验
+    formallist = [] #学历
+    for i in detaillist:
+        zonelist.append(i[0])
+        experiencelist.append(i[1])
+        formallist.append(i[2])
+    df = pd.DataFrame({'zone':zonelist,
+                       'jobs':jobslist,
+                       'salary':salarylist,
+                       'experience':experiencelist,
+                       'formal':formallist})
+    return df
+print(request(url))
 
