4b99580b05f8dec42cc4a9a40723dd6d66dc28a2
Muzi-Li-China <38267715+Muzi-Li-China@users.noreply.github.com>
2018-05-05 09:55:31 +0800

    Update 2018-05-05-GUI.py

--- 2018-05-05-GUI.py
+++ 2018-05-05-GUI.py
@@ -4,16 +4,14 @@ class MyFrame(wx.Frame):
         wx.Frame.__init__(self,None,-1,'花名册',size=(520,450))
         panel = wx.Panel(self)
         #静态标签
-        labeAll = wx.StaticText(panel,
+        labelAll = wx.StaticText(panel,
                                 -1,
                                 '记录',
-                                pos=(220,5),
                                 )
         #文本框
         self.textAll = wx.TextCtrl(panel,
                                    -1,
                                    size = (480,200),
-                                   pos = (10,25),
                                    style=wx.TE_MULTILINE | wx.TE_READONLY) #文本框的属性
         labelName = wx.StaticText(panel,
                                   -1,
@@ -23,31 +21,49 @@ class MyFrame(wx.Frame):
         self.textName = wx.TextCtrl(panel,
                                     -1,
                                     size = (300,25),
-                                    pos = (40,230),
                                     )
         labelSex = wx.StaticText(panel,
                                  -1,
                                  '性别',
-                                 pos = (10,265),
                                  )
         self.textSex = wx.TextCtrl(panel,
                                    -1,
                                    size = (300,25),
-                                   pos = (40,260),
                                    )
         labelAge = wx.StaticText(panel,
                                  -1,
                                  '年龄',
-                                 pos =(10,295),
                                  )
         self.textAge = wx.TextCtrl(panel,
                                    -1,
                                    size = (300,25),
-                                   pos = (40,290),
                                    )
-        self.btnPut = wx.Button(panel,-1,'提交',size=(75,25),pos = (220,350)) #添加按钮
+        self.btnPut = wx.Button(panel,-1,'提交',size=(75,25)) #添加按钮
         self.Bind(wx.EVT_BUTTON,self.OnButtonPut,self.btnPut) #响应事件
-        
+
+        #布局管理器
+        nameSizer = wx.BoxSizer()
+        nameSizer.Add(labelName, proportion=0,flag=wx.ALIGN_CENTER)
+        nameSizer.Add(self.textName, proportion=0,flag=wx.ALIGN_CENTER)
+
+        sexSizer = wx.BoxSizer()
+        sexSizer.Add(labelSex, proportion=0, flag=wx.ALIGN_CENTER)
+        sexSizer.Add(self.textSex, proportion=0, flag=wx.EXPAND)
+
+        ageSizer = wx.BoxSizer()
+        ageSizer.Add(labelAge, proportion=0, flag=wx.ALIGN_CENTER)
+        ageSizer.Add(self.textAge, proportion=0, flag=wx.EXPAND)
+
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        mainSizer.Add(labelAll, proportion=0,flag=wx.ALIGN_CENTER)
+        mainSizer.Add(self.textAll, proportion=0,flag=wx.EXPAND)
+        mainSizer.Add(nameSizer, proportion=0, flag=wx.ALIGN_CENTER)
+        mainSizer.Add(sexSizer, proportion=0, flag=wx.ALIGN_CENTER)
+        mainSizer.Add(ageSizer, proportion=0, flag=wx.ALIGN_CENTER)
+        mainSizer.Add(self.btnPut, proportion=0, flag=wx.ALIGN_CENTER)
+
+        panel.SetSizer(mainSizer)
+
     #事件函数
     def OnButtonPut(self,event):
         name = self.textName.GetValue()
